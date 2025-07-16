e9a99e64d3add5a1b57f3ae6123a567e54b5e5a0
zpnst <akuratnonenado@gmail.com>
2025-07-16 19:32:40 +0300

    configy Unmarshal

---
+++ linux/types.go
@@ -0,0 +1,36 @@
+package linux
+
+type Configy struct {
+	Version   string    `json:"version"`
+	Cmd       string    `json:"cmd"`
+	Isolation Isolation `json:"isolation"`
+	UserNS    UserNS    `json:"userns"`
+}
+
+type Isolation struct {
+	Cgroups    []CgroupsUint `json:"cgroups"`
+	Namespaces []string      `json:"namespaces"`
+}
+
+type CgroupsUint struct {
+	Type     string `json:"type"`
+	Resource string `json:"resource"`
+	Value    string `json:"value"`
+}
+
+type UserNS struct {
+	User        UserUnit    `json:"user"`
+	UidMappings MappingUint `json:"uidMappings"`
+	GidMappings MappingUint `json:"gidMappings"`
+}
+
+type UserUnit struct {
+	UID int `json:"uid"`
+	GID int `json:"gid"`
+}
+
+type MappingUint struct {
+	ContainerID int `json:"containerID"`
+	HostID      int `json:"hostID"`
+	Size        int `json:"size"`
+}
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+
+	"github.com/zpnst/containy/linux"
+)
+
+const (
+	configyPath string = "bundle/configy.json"
+)
 
 func main() {
-	fmt.Println("Hello, containy!")
+	f, err := os.Open(configyPath)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer f.Close()
+
+	b, err := io.ReadAll(f)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	var configy linux.Configy
+	if err := json.Unmarshal(b, &configy); err != nil {
+		log.Panicln(err)
+	}
+
+	log.Printf("%+v", configy)
 }
